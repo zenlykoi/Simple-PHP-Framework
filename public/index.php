5831c82ea08185525cc17d27612a439a219bb393
phuong <42612129+zenlykoi@users.noreply.github.com>
2020-10-24 14:58:05 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,9 +2,9 @@
 
 define('ROOT_FOLDER',dirname(__DIR__, 1));
 
-require_once ROOT_FOLDER . '/settings/setting.php';
 require_once ROOT_FOLDER . '/settings/define.php';
 require_once ROOT_FOLDER . '/settings/config.php';
+require_once ROOT_FOLDER . '/settings/setting.php';
 require_once ROOT_FOLDER . '/vendor/autoload.php';
 require_once ROOT_FOLDER . '/libs/Autoload/autoload.php';
 require_once ROOT_FOLDER . '/core/router/router.php';
